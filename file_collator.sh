793ab17526ea698418017eb8cee7b22f1d9cbdc3
Adam Hartz <ahartz1@gmail.com>
2015-10-01 15:36:14 -0400

    Create bash script to cp all desired exensions to data folder

---
+++ file_collator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Start by finding all files with the desired extensions
+find . \
+      \(   -name '*.c' -or -name '*.gcc'                        `# C` \
+       -or -name '*.csharp'                                    `# C#` \
+       -or -name '*.sbcl'                                      `# Common Lisp` \
+       -or -name '*.clojure'                                   `# Clojure` \
+       -or -name '*.ghc'                                       `# Haskell GHC` \
+       -or -name '*.java'                                      `# Java` \
+       -or -name '*.js'                                        `# JavaScript` \
+       -or -name '*.ocaml'                                     `# OCaml` \
+       -or -name '*.perl'                                      `# Perl` \
+       -or -name '*.hack' -or -name '*.php'                    `# PHP` \
+       -or -name '*.py' -or -name '*.python3'                  `# Python` \
+       -or -name '*.rb' -or -name '*.jruby' -or -name '*.yarv' `# Ruby` \
+       -or -name '*.scala'                                     `# Scala` \
+       -or -name '*.racket'  \)                                `#scheme` \
+       -exec cp "{}" ~/TIY/programming-language-classifier/data/ \;
