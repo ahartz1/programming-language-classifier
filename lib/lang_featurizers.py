afe6654fac9d7f726cf57a43f42ba5013cbaadc6
Adam Hartz <ahartz1@gmail.com>
2015-10-02 23:07:58 -0400

    Update regex to skip newline groups (?:^|\n)

--- lib/lang_featurizers.py
+++ lib/lang_featurizers.py
@@ -58,62 +58,55 @@ class FunctionFeaturizer(TransformerMixin):
 
 # Differentiating types of null values
 # Ruby
-def percent_nil(text):
-    total_length = len(text)
+def presence_nil(text):
     result = re.findall(r'\W+nil\W+', text)
     if result:
-        return len(result) / total_length
+        return 1
     return 0
 
 
-def percent_nil_caps(text):
-    total_length = len(text)
+def presence_nil_caps(text):
     result = re.findall(r'\W+NIL\W+', text)
     if result:
-        return len(result) / total_length
+        return 1
     return 0
 
 
-def percent_null(text):
-    total_length = len(text)
+def presence_null(text):
     result = re.findall(r'\W+null\W+', text)
     if result:
-        return len(result) / total_length
+        return 1
     return 0
 
 
-def percent_none(text):
-    total_length = len(text)
+def presence_none(text):
     result = re.findall(r'\W+None\W+', text)
     if result:
-        return len(result) / total_length
+        return 1
     return 0
 
 
 # Differentiating types of code comments
 # Scheme
-def percent_start_double_semicolons(text):
-    total_length = len(text)
+def presence_start_double_semicolons(text):
     result = re.findall(r'(^;;) | (\n;;)', text)
     if result:
-        return len(result) / total_length
+        return 1
     return 0
 
 
-def percent_start_hashes(text):
-    total_length = len(text)
+def presence_start_hashes(text):
     result = re.findall(r'(^#) | (\n#)', text)
     if result:
-        return len(result) / total_length
+        return 1
     return 0
 
 
 
-def percent_bar_hash(text):
-    total_length = len(text)
+def presence_bar_hash(text):
     result = re.findall(r'(\n\|\#) | (\#\|\n)', text)
     if result:
-        return len(result) / total_length
+        return 1
     return 0
 
 
@@ -127,48 +120,44 @@ def percent_start_and_end_parenthesis(text):
 
 
 # Specific to C#
-def percent_void(text):
-    total_length = len(text)
+def presence_void(text):
     result = re.findall(r'\s+void\s+', text)
     if result:
-        return len(result) / total_length
+        return 1
     return 0
 
 
-def percent_public(text):
-    total_length = len(text)
+def presence_public(text):
     result = re.findall(r'\s+public\s+', text)
     if result:
-        return len(result) / total_length
+        return 1
     return 0
 
 
-def percent_bool(text):
-    total_length = len(text)
+def presence_bool(text):
     result = re.findall(r'\s+bool\s+', text)
     if result:
-        return len(result) / total_length
+        return 1
     return 0
 
-
-def percent_int(text):
-    total_length = len(text)
+# NOT USED DUE TO LOWERED SCORES!
+def presence_int(text):
     result = re.findall(r'\s+int\s+', text)
     if result:
-        return len(result) / total_length
+        return 1
     return 0
 
 
 # Specific to Ruby
 def presence_module_line(text):
-    result = re.findall(r'(^|\n)\s*module ([A-Z][a-z]*)+', text)
+    result = re.findall(r'(?:^|\n)\s*module ([A-Z][a-z]*)+', text)
     if result:
         return 1
     return 0
 
 
 def presence_extend_line(text):
-    result = re.findall(r'(^|\n)\s*extend ([A-Z][a-z]*)+::([A-Z][a-z]*)+',
+    result = re.findall(r'(?:^|\n)\s*extend ([A-Z][a-z]*)+::([A-Z][a-z]*)+',
                         text)
     if result:
         return 1
@@ -176,38 +165,50 @@ def presence_extend_line(text):
 
 
 def presence_require_line(text):
-    result = re.findall(r'(^|\n)\s*require .+', text)
+    result = re.findall(r'(?:^|\n)\s*require .+', text)
     if result:
         return 1
     return 0
 
 
 def presence_end(text):
-    result = re.findall(r'(^|\n)\s*end\s*($|\n)', text)
+    result = re.findall(r'(?:^|\n)\s*end\s*(?:$|\n)', text)
+    if result:
+        return 1
+    return 0
+
+
+def presence_multiple_end(text):
+    result = re.findall(r'(\n\s*end\s*(?:$|\n)){2,}', text)
     if result:
         return 1
     return 0
 
 
 def presence_def_no_colon(text):
-    result = re.findall(r'((^|\n)\s*def[^:]*($|\n)){1}', text)
+    result = re.findall(r'((?:^|\n)\s*def[^:]*(?:$|\n)){1}', text)
     if result:
         return 1
     return 0
 
 
-def percent_at(text):
-    total_length = len(text)
-    result = re.findall(r'@', text)
+def presence_at(text):
+    result = re.findall(r'@[\w]+', text)
     if result:
-        return len(result) / total_length
+        return 1
+    return 0
+
+
+def presence_double_at(text):
+    result = re.findall(r'@@[\w]+', text)
+    if result:
+        return 1
     return 0
 
 
 # Specific to Clojure
-def presence_defn_no_colon(text):
-    result = re.findall(r'(^|\n)\s*\(\s*defn [^:\dA-Z]*($|\n)',
-                        text)
+def presence_defn(text):
+    result = re.findall(r'(?:^|\n)\s*\(\s*defn', text)
     if result:
         return 1
     return 0
@@ -215,10 +216,76 @@ def presence_defn_no_colon(text):
 
 def percent_consecutive_closing_paren(text):
     total_length = re.findall(r'($|\n)', text)
-    result = re.findall(r'\){2,}($|\n)', text)
+    result = re.findall(r'\){2,}(?:$|\n)', text)
     if result:
         return len(result) / len(total_length)
     return 0
+
+
+# Specific to Python
+def presence_from_import_line(text):
+    result = re.findall(r'(?:^|\n)\s*from\s+[a-z_\.]+\s+import\s+[\w\.*]+',
+                        text)
+    if result:
+        return 1
+    return 0
+
+
+def presence_import_line(text):
+    result = re.findall(r'(?:^|\n)\s*import\s+[a-z_\.*]+', text)
+    if result:
+        return 1
+    return 0
+
+
+def presence_print(text):
+    result = re.findall(r'\n\s*print\(', text)
+    if result:
+        return 1
+    return 0
+
+
+def presence_dot_join(text):
+    result = re.findall(r'\.join\(', text)
+    if result:
+        return 1
+    return 0
+
+
+def presence_dot_format(text):
+    result = re.findall(r'(\'|")\.format\(', text)
+    if result:
+        return 1
+    return 0
+
+
+def presence_dot_values(text):
+    result = re.findall(r'\.values\(\)', text)
+    if result:
+        return 1
+    return 0
+
+
+def presence_dunder_name(text):
+    result = re.findall(r'\nif __name__ == \'__main__\':\n', text)
+    if result:
+        return 1
+    return 0
+
+
+def presence_dunder_init(text):
+    result = re.findall(r'(?:^|\n)\s*def __init__\(self.*\):\n', text)
+    if result:
+        return 1
+    return 0
+
+
+def presence_def_colon(text):
+    result = re.findall(r'^|\ndef .*:\n', text)
+    if result:
+        return 1
+    return 0
+
 
 
 
